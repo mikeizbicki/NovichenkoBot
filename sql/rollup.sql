e0bab75af59d3766f0a5a01a05e1e59fc89660a5
Mike Izbicki <mike@izbicki.me>
2019-12-18 16:54:31 -0800

    fix off by one error

--- sql/rollup.sql
+++ sql/rollup.sql
@@ -32,7 +32,7 @@ BEGIN
      * We do a SELECT .. FOR UPDATE on the row in the rollup table to prevent
      * aggregations from running concurrently.
      */
-    SELECT event_table_name, last_aggregated_id+1, LEAST(last_aggregated_id+max_rollup_size,pg_sequence_last_value(event_id_sequence_name))
+    SELECT event_table_name, last_aggregated_id+1, LEAST(last_aggregated_id+max_rollup_size+1,pg_sequence_last_value(event_id_sequence_name))
     INTO table_to_lock, window_start, window_end
     FROM rollups
     WHERE name = rollup_name FOR UPDATE;
@@ -63,7 +63,7 @@ BEGIN
     /*
      * Remember the end of the window to continue from there next time.
      */
-    UPDATE rollups SET last_aggregated_id = window_end WHERE name = rollup_name;
+    UPDATE rollups SET last_aggregated_id = window_end-1 WHERE name = rollup_name;
 END;
 $function$;
 
@@ -166,4 +166,50 @@ VALUES ('responses_timestamp_hostname', 'responses', 'responses_id_responses_seq
     ;
 $$);
 
-;
+CREATE TABLE responses_summary (
+    timestamp TIMESTAMP,
+    bytes BIGINT NOT NULL,
+    num BIGINT NOT NULL,
+    num_http_200 BIGINT NOT NULL,
+    num_http_3xx BIGINT NOT NULL,
+    num_http_4xx BIGINT NOT NULL,
+    num_twisted_fail BIGINT NOT NULL,
+    PRIMARY KEY (timestamp)
+);
+INSERT INTO rollups (name, event_table_name, event_id_sequence_name, sql)
+VALUES ('responses_summary', 'responses', 'responses_id_responses_seq', $$
+    INSERT INTO responses_summary
+        ( timestamp
+        , bytes
+        , num
+        , num_http_200
+        , num_http_3xx
+        , num_http_4xx
+        , num_twisted_fail
+        )
+    SELECT
+        date_trunc('minute',timestamp_received) as timestamp,
+        sum(CASE WHEN bytes is null THEN 0 ELSE bytes END) as bytes,
+        count(1) as num,
+        sum(CASE WHEN http_status = '200' THEN 1 ELSE 0 END) as num_http_200,
+        sum(CASE WHEN http_status >= '300' and http_status < '400' THEN 1 ELSE 0 END) as num_http_3xx,
+        sum(CASE WHEN http_status >= '400' and http_status < '500' THEN 1 ELSE 0 END) as num_http_4xx,
+        sum(CASE WHEN twisted_status != 'Success' THEN 1 ELSE 0 END) as num_twisted_fail
+    FROM responses
+    WHERE 
+        id_responses>=$1 AND
+        id_responses<$2
+    GROUP BY timestamp
+    ON CONFLICT (timestamp)
+    DO UPDATE SET 
+        bytes = responses_summary.bytes+excluded.bytes,
+        num = responses_summary.num+excluded.num,
+        num_http_200 = responses_summary.num_http_200+excluded.num_http_200,
+        num_http_3xx = responses_summary.num_http_3xx+excluded.num_http_3xx,
+        num_http_4xx = responses_summary.num_http_4xx+excluded.num_http_4xx,
+        num_twisted_fail = responses_summary.num_twisted_fail+excluded.num_twisted_fail
+    ;
+$$);
+
+CREATE VIEW total_bytes AS
+    SELECT pg_size_pretty(sum(bytes)) FROM responses_summary;
