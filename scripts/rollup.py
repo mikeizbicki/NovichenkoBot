e700990274314f0b3a4492dccc1bbde2af4a627a
Mike Izbicki <mike@izbicki.me>
2019-12-28 20:41:26 -0800

    begin change on rollup script

--- scripts/rollup.py
+++ scripts/rollup.py
@@ -3,8 +3,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--db',default='postgres:///novichenkobot')
 parser.add_argument('--max_rollup_size',type=int,default=1000000)
+parser.add_argument('--unsafe',action='store_true')
 parser.add_argument('--name',required=True)
 args = parser.parse_args()
+
+force_safe=True
+if args.unsafe:
+    force_safe=False
 
 # imports
 import sqlalchemy
@@ -25,19 +30,29 @@ rows_rolled=0
 while True:
     print(datetime.datetime.now(),'rows_rolled=',rows_rolled)
 
-    sql = sqlalchemy.sql.text('''
-        SELECT * FROM do_rollup(:name,:max_rollup_size);
-    ''')
-    res = connection.execute(sql,{
-        'name':args.name,
-        'max_rollup_size':args.max_rollup_size
-        })
-    row = res.first()
-    new_rows = row[1]-row[0]
-    rows_rolled += new_rows
+    if args.name is None:
+        sql = sqlalchemy.sql.text('''
+            SELECT name from rollups;
+        ''')
+        res = connection.execute(sql)
+        rollup_tables = list(res)
+        print('rollup_tables=',rollup_tables)
 
-    if new_rows < args.max_rollup_size:
-        break
+    with connection.begin() as trans:
+        sql = sqlalchemy.sql.text('''
+            SELECT * FROM do_rollup(:name,:max_rollup_size,:force_safe);
+        ''')
+        res = connection.execute(sql,{
+            'name':args.name,
+            'max_rollup_size':args.max_rollup_size,
+            'force_safe':force_safe
+            })
+        row = res.first()
+        new_rows = row[1]-row[0]
+        rows_rolled += new_rows
+
+        if new_rows < args.max_rollup_size:
+            break
 
 # close the connection
 connection.close()
