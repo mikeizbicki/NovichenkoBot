4bdd18f51bea9970695909dec464202ba3a3a8fd
Mike Izbicki <mike@izbicki.me>
2019-12-30 23:18:58 -0800

    breadth first run_crawlers

--- run_crawlers.sh
+++ run_crawlers.sh
@@ -8,7 +8,7 @@ set -e
 
 db=novichenkobot
 db_rfc=postgres:///$db
-num_jobs=80
+num_jobs=100
 
 ########################################
 # create log directories
@@ -34,7 +34,7 @@ hostnames_high=$(echo "$res" | tail -n +4 | head -n -3)
 
 for hostname in $hostnames_high; do
     echo "high priority crawl: $hostname"
-    nice -n -20 scrapy crawl general -s HOSTNAME_RESTRICTIONS=$hostname -a db=$db_rfc > $log/general-$hostname 2>&1 &
+    nice -n -10 scrapy crawl general -s HOSTNAME_RESTRICTIONS=$hostname -a db=$db_rfc > $log/general-$hostname 2>&1 &
     echo $! >> $log/pids
 done
 
@@ -46,10 +46,27 @@ res=$(psql $db -c "
 select hostname 
 from crawlable_hostnames 
 where 
-    priority!='high' and 
+    priority='' and 
     (lang='en' or lang='')
 order by hostname;
 ")
+res=$(psql $db -c "
+select hostname_target from (
+select hostname_target,sum(num) as num
+from refs_keywords
+where
+    type='link' and
+    hostname_source in (select hostname from hostname_productivity limit 500) and
+    hostname_target not in (select hostname from crawlable_hostnames) and
+    right(hostname_target, length(hostname_target)-4) not in (SELECT hostname FROM crawlable_hostnames) and
+    hostname_target not in (select hostname from responses_timestamp_hostname)
+group by hostname_target
+order by num desc
+) as t1
+-- offset $(( 25 * $num_jobs ))
+limit $(( 5 * $num_jobs ));
+")
+
 hostnames_low=$(echo "$res" | tail -n +4 | head -n -3)
 
 i=0
