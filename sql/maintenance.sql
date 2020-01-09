e0d07da153e01854512603b4506a6052a5a998a9
Mike Izbicki <mike@izbicki.me>
2020-01-09 15:55:54 -0800

    sql/maintenance.sql

--- sql/maintenance.sql
+++ sql/maintenance.sql
@@ -105,6 +105,27 @@ JOIN pg_stat_activity blockinga ON blockingl.pid = blockinga.pid
 WHERE NOT blockedl.granted
 AND blockinga.datname = current_database();
 
+-- cancel all blocked queries
+SELECT pg_cancel_backend(blocked_pid)
+FROM (
+    SELECT
+      COALESCE(blockingl.relation::regclass::text,blockingl.locktype) as locked_item,
+      now() - blockeda.query_start AS waiting_duration, blockeda.pid AS blocked_pid,
+      blockeda.query as blocked_query, blockedl.mode as blocked_mode,
+      blockinga.pid AS blocking_pid, blockinga.query as blocking_query,
+      blockingl.mode as blocking_mode
+    FROM pg_catalog.pg_locks blockedl
+    JOIN pg_stat_activity blockeda ON blockedl.pid = blockeda.pid
+    JOIN pg_catalog.pg_locks blockingl ON(
+      ( (blockingl.transactionid=blockedl.transactionid) OR
+      (blockingl.relation=blockedl.relation AND blockingl.locktype=blockedl.locktype)
+      ) AND blockedl.pid != blockingl.pid)
+    JOIN pg_stat_activity blockinga ON blockingl.pid = blockinga.pid
+      AND blockinga.datid = blockeda.datid
+    WHERE NOT blockedl.granted
+    AND blockinga.datname = current_database()
+)t;
+
 /*
  * This query finds long running queries;
  * autovacuum often takes a long time and causes the rollup queries to stall
