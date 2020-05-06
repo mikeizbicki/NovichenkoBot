dd1cc6a527efb041120aa57b6c7faeab220a5a63
Mike Izbicki <mike@izbicki.me>
2020-05-06 13:18:35 -0700

    clean up code

--- run_crawlers_2.sh
+++ run_crawlers_2.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-#set -e
-
 # run like a deamon
 if [ "$1" != 'daemon_mode' ]; then
 
     # find and kill previous daemon
     pids=$(ps -ef | grep 'run_crawlers_2.sh daemon_mode' | grep -v grep | cut -c9-15)
-    kill $pids > /dev/null 2>&1
+    kill $pids > /dev/null 2>&1 || true
 
     # launch in daemon mode and exit non-daemon mode
     nohup $0 'daemon_mode' >> log/run_crawlers_2 2>&1 &
@@ -15,20 +13,24 @@ if [ "$1" != 'daemon_mode' ]; then
 fi
 
 # the main loop
-offset=0
+iteration=0
 while true; do
-    date +"%Y-%m-%d_%H-%M-%S: next iteration, offset=$offset"
+
+    # log time and iteration number
+    date +"%Y-%m-%d_%H-%M-%S: next iteration, iteration=$iteration"
+
+    # get pids of previous crawlers to cancel them later
+    # by waiting to cancel them, we get less downtime if it takes a while to start new crawlers
     prev_pids=$(ps -ef | grep scrapy | cut -c9-15)
-    #select pid,application_name,pg_cancel_backend(pid) from pg_stat_activity where application_name='NovichenkoBot_GeneralSpider';
-    #cat log/newest/pids | xargs kill
-    #cat log/newest/pids | xargs kill
+
+    # launch new crawlers
     sh run_crawlers.sh $offset
-    offset=$(( $offset + 150 ))
-    echo $prev_pids | xargs kill > /dev/null 2>&1
-    echo $prev_pids | xargs kill > /dev/null 2>&1
+    offset=$(( $offset + 1 ))
+
+    # kill previous crawlers
+    kill $prev_pids > /dev/null 2>&1 || true
+    kill $prev_pids > /dev/null 2>&1 || true
+
+    # sleep
     sleep 20m
-    #sh run_rollups.sh
-    #sh run_views.sh
-    #psql novichenkobot -c 'refresh materialized view hostname_productivity'
-    #sleep 30m
 done
